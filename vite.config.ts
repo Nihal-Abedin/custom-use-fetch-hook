3259a0a7133308c91ba7e3283c94b3a4554c3c19
Nihal-Abedin <nihal.abedin.annon99@gmail.com>
2024-10-12 02:01:15 +0600

    updated for npm package

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+        entry: 'src/hooks/useFetchFactory.ts', // Entry file for your library
+        name: 'useFetch', // Global variable name for UMD builds
+        fileName: (format) => `use-fetch-hook.${format}.ts`, // Output filename
+    },
+    rollupOptions: {
+        // Make sure to externalize dependencies that shouldn't be bundled
+        external: ['react', 'react-dom'],
+        output: {
+            globals: {
+                react: 'React',
+                'react-dom': 'ReactDOM',
+            },
+        },
+    },
+},
 })
